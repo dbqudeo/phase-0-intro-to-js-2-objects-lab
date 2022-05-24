ae6df220fccbc43a537e5e8f0205faf82a87939d
byeongdae yu <brianbdyu@gmail.com>
2022-05-23 21:54:43 -0400

    commit tree 86ba01c080536d2c33dca2d2003141868b298710

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+const employee = {
+    name: "Brian",
+    address: "123 broad ave"
+}
+
+function  updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+    
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = { ...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+/*
+  describe('destructivelyDeleteFromEmployeeByKey(employee, key)', function () {
+    it('returns employee without the deleted key/value pair', function () {
+      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+      expect(newEmployee['name']).to.equal(undefined);
+    });
+
+    it('modifies the original employee', function () {
+      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+      expect(employee['name']).to.equal(undefined);
+      expect(employee).to.equal(newEmployee);
+    });
+  });
+});
+*/
